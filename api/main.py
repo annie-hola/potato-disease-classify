b5483e09a2dc3cd624b5162b47aa07836445aadc
Ha Kieu Anh <hakieuanh@Annies-Mac.local>
2024-01-06 17:31:46 +0700

    api fastapi

---
+++ api/main.py
@@ -0,0 +1,59 @@
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware( # config request middleware
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("../saved_models/1")
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"] # type of class + align with classes output
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray: # turn image -> bytes array
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0) # turn 1dims -> 2 dims
+     
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class, # predicted class
+        'confidence': float(confidence) # percentage right
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
+# Run FastAPI app: uvicorn main:app --reload --host 0.0.0.0
+# Docs: https://fastapi.tiangolo.com/#installation
+
